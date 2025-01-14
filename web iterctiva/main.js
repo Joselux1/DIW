bf24c8d796acfb339c785d6eace778798ad87be7
Joselux1 <160737007+Joselux1@users.noreply.github.com>
2025-01-14 23:11:28 +0100

    Primera Subida

---
+++ web iterctiva/main.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", (event) => {
+
+const cambiarTema = document.getElementById("cambiarColor");
+const body = document.getElementById("default");
+
+
+cambiarTema.addEventListener("click", function () {
+    // Alterna entre las clases "dark-mode" y "light-mode"
+    if (body.classList.contains("modo-Claro")) {
+      body.classList.remove("modo-Claro");
+      body.classList.add("modo-Oscuro");
+      footer.classList.add("footer-claro");
+
+    } else {
+      body.classList.remove("modo-Oscuro");
+      body.classList.add("modo-Claro");
+      
+    }
+  });
+
+
+
+
+
+
+
+
+});
